b635819e94d21a6d09fc781623cc8f5b0cf13f38
Sebastian Soto <s.m.sebastian.n@gmail.com>
2020-09-11 16:47:48 -0600

    Patch: font2, Fix: config.def.h fonts

--- config.def.h
+++ config.def.h
@@ -5,7 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+    "Monofur Nerd Font:size=12:antialias=true:autohint=true"
+    "Symbola:pixelsize=15:antialias=true:autohint=true", 
+};
+
 static int borderpx = 2;
 
 /*
--- config.h
+++ config.h
@@ -5,7 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+    "Monofur Nerd Font:size=12:antialias=true:autohint=true"
+    "Symbola:pixelsize=15:antialias=true:autohint=true", 
+};
+
 static int borderpx = 2;
 
 /*
@@ -94,7 +100,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.7;
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
