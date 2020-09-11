0449cf11e267c545599e671aa6396b6b8dfaa460
Sebastian Soto <s.m.sebastian.n@gmail.com>
2020-09-11 17:17:04 -0600

    Fix: Cursor & BG colors

--- config.h
+++ config.h
@@ -138,8 +138,8 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
